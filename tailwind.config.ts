0e31519542f152d6a87bd3c1037165e9cc9c58e8
unknown <you@example.com>
2024-12-19 19:17:39 +0500

    Add Image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        header: "#D9D9D9",
+        button: "#121212",
+        para: "#4C4C4C",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        '44px': '44px',
+        '32px': '32px',
+        '28px': '28px',
+      },
+      fontWeight: {
+        '400': '400', 
+        '700': '700', 
+       
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
